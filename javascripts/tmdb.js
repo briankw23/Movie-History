a6b77ad3cb6176b8a4854dfcd7650a6cfd09300f
Brian Wright <briankw23@gmail.com>
2018-05-26 09:48:01 -0500

    images are printing

--- javascripts/tmdb.js
+++ javascripts/tmdb.js
@@ -3,8 +3,33 @@ const dom = require('./dom');
 
 let tmdbKey = '';
 
+let imageConfig = {};
+
 const setKey = (key) => {
   tmdbKey = key;
+  getConfig();
+};
+
+const getConfig = () => {
+  tmdbConfiguration()
+    .then((result) => {
+      imageConfig = result.images;
+    })
+    .catch((err) => {
+      console.error('error with tmdb config:', err);
+    });
+};
+
+const tmdbConfiguration = () => {
+  return new Promise((resolve, reject) => {
+    $.ajax(`https://api.themoviedb.org/3/configuration?api_key=${tmdbKey}`)
+      .done((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 };
 
 const searchTMDB = (txt) => {
@@ -23,7 +48,7 @@ const showResults = (searchText) => {
   // dom.domString([singleMovie,singleMovie,singleMovie,singleMovie,]);
   searchTMDB(searchText)
     .then((result) => {
-      dom.domString(result);
+      dom.domString(result, imageConfig);
     })
     .catch((err) => {
       console.error('search error', err);
